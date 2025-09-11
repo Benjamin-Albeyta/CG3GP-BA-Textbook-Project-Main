ff475097c81c1809633ad25e98a36eab565f19ab
Benjamin-Albeyta <balbeyta@gmail.com>
2025-09-11 16:33:25 -0400

    Finished the Chapter 05 in the textbook

--- Assets/Scripts/MyFirstScript.cs
+++ Assets/Scripts/MyFirstScript.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class MyFirstScript : MonoBehaviour
 {
+
+    [SerializeField]
+    private float speed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("test Start");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        print(speed);
+
     }
 }
